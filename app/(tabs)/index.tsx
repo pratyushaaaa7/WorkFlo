ca4fe92b8e470050fb28d1b0e491f899c3bd511e
pratyusha <pratyushaaaa7@gmail.com>
2025-08-01 16:58:30 +0530

    some changes check

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Prata</ThemedText>
+        <ThemedText type="title">Welcome Pratyushaa</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
