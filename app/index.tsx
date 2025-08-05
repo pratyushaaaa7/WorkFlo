bd99de3b5dd2b261e1c1f242eb4ad1d2430426c6
pratyusha <pratyushaaaa7@gmail.com>
2025-08-05 13:43:43 +0530

    some more changes to set up

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Home() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to Home Page</Text>
+      <Text>Welcome to Home Pageee</Text>
     </View>
   );
 }
