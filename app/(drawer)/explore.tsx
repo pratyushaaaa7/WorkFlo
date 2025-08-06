7d945c438d4a97cb58bd122ece47aad2f5093473
pratyusha <pratyushaaaa7@gmail.com>
2025-08-06 12:35:51 +0530

    push

--- app/(drawer)/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function ExploreScreen() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold">Explore Page</Text>
-    </View>
-  );
-}
