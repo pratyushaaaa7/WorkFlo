1d65d42108252e45a81a5c7f25261b04d0d70473
pratyusha <pratyushaaaa7@gmail.com>
2025-12-04 12:16:45 +0530

    push rpouting in svr and dpr

--- app/dprs.tsx
+++ app/dprs.tsx
@@ -109,7 +109,7 @@ const DPRs = () => {
       <LinearGradient colors={["#6366F1", "#8B5CF6"]}>
         <View className="pt-16 pb-6 px-4 flex-row items-center justify-between shadow-md">
           <TouchableOpacity
-            onPress={() => router.back()}
+            onPress={() => router.push("/projectMain")}
             className="flex-row items-center"
             activeOpacity={0.7}
           >
