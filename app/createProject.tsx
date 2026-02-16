17e4c995a4be98bfc645315f325a76fc2d48fd6d
pratyusha <pratyushaaaa7@gmail.com>
2026-02-16 16:32:04 +0530

    fixed expo warning in in create project iomage upload

--- app/createProject.tsx
+++ app/createProject.tsx
@@ -177,7 +177,7 @@ const CreateProjectScreen = () => {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ["images"],
       allowsMultipleSelection: true,
       quality: 0.8,
     });
